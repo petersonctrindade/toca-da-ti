5b36e690ac81c9b99a8124bd9e71bb541a897ebc
marianasvlt <dhystie@gmail.com>
2024-12-06 14:12:06 -0300

    Update faq.js

--- js/faq.js
+++ js/faq.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const acordeaoButton = document.querySelectorAll(".acordeao-btn");
 
+    acordeaoButton.forEach(button => {
+        button.addEventListener("click", () => {
+       const conteudo = button.nextElementSibling;
+       button.classList.toggle("active");
+       if (conteudo.style.maxHeight) {
+           conteudo.style.maxHeight = null;
+       } else {
+           conteudo.style.maxHeight = conteudo.scrollHeight + "px";
+       }
+        });
+    });
+});
