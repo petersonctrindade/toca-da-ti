91c6fc903668ec0670f3004396b05a069520754f
Érica Pereira <161359853+hpHellen@users.noreply.github.com>
2024-12-11 16:55:30 -0300

    Update profissoes.js

--- js/profissoes.js
+++ js/profissoes.js
@@ -2,12 +2,16 @@ const links = document.querySelectorAll(".link-profissoes")
 
 links.forEach(link => {
     link.addEventListener("click", () => {
-        if (link.id === "link-dev") {
+        if (link.id === "link-design") {
+            document.getElementById("area-design").scrollIntoView({behavior: 'smooth'})
+        } else if (link.id === "link-eng") {
+            document.getElementById("area-eng").scrollIntoView({behavior: 'smooth'})
+        } else if (link.id === "link-cyber") {
+            document.getElementById("area-cyber").scrollIntoView({behavior: 'smooth'})
+        }else if(link.id === "link-sup"){
+            document.getElementById("area-suporte").scrollIntoView({behavior: 'smooth'})
+        }else if(link.id === "link-dev"){
             document.getElementById("area-dev").scrollIntoView({behavior: 'smooth'})
-        } else if (link.id === "link-dados") {
-            document.getElementById("area-dados").scrollIntoView({behavior: 'smooth'})
-        } else if (link.id === "link-redes") {
-            document.getElementById("area-redes").scrollIntoView({behavior: 'smooth'})
         }
     })
 });
